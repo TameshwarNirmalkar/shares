2ac9a223294057114c00329393fb6f348cb37296
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2024-02-12 18:40:28 +0530

    change package json node version

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import { FC, memo, useCallback, useEffect, useState } from "react";
 
 import { Metadata } from "next";
+import { useRouter } from "next/navigation";
 
 const metadata: Metadata = {
   title: "Dashbard Page",
@@ -12,6 +13,7 @@ const metadata: Metadata = {
 
 const Dashboard: FC<{}> = memo(() => {
   const { data: session }: any = useSession();
+  const { replace } = useRouter();
   const [userList, setUserList] = useState<any[]>([]);
   const [errormsg, setErrorMsg] = useState<string>("");
 
@@ -45,10 +47,27 @@ const Dashboard: FC<{}> = memo(() => {
     <>
       {errormsg && <Alert message="Unauthorised" description="Please login again." type="error" />}
       <Row gutter={[10, 10]}>
-        {userList?.map((el) => (
+        {/* {userList?.map((el) => (
           <Col span={8} key={el._id}>
             <Card title={el.full_name} bordered={true}>
               {el.email}
+            </Card>
+          </Col>
+        ))} */}
+        {[
+          { title: "Dashboard", rpath: "/dashboard" },
+          { title: "Add Clients", rpath: "/add-clients" },
+          { title: "Clients List", rpath: "/clients-list" },
+          { title: "My Investments", rpath: "/my-investments" },
+          { title: "Stakeholder", rpath: "/stakeholder" },
+          { title: "Investment List", rpath: "/investment-list" },
+          { title: "Transaction History", rpath: "/transaction-history" },
+          { title: "Interest Calculator", rpath: "/interest-calculator" },
+          { title: "Investment", rpath: "/investment" },
+        ].map((el: any, ind: number) => (
+          <Col span={8} key={ind}>
+            <Card title={el.title} bordered={true} onClick={() => replace(`${el.rpath}`)} className="cursor-pointer">
+              {el.rpath}
             </Card>
           </Col>
         ))}
