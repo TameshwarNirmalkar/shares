fd75a6ed020551fd6a01e9698c6385c084532b16
Tameshwar Nirmalkar <tameshwar.nirmalkar@gmail.com>
2024-02-21 21:44:48 +0530

    register page

--- src/components/RegisterComponent.tsx
+++ src/components/RegisterComponent.tsx
@@ -36,46 +36,49 @@ const RegisterForm: FC<{}> = memo(() => {
   }, []);
 
   return (
-    <div>
-      {contextHolder}
-      <Form className="space-y-4 md:space-y-6" layout="vertical" name="registrationForm" onFinish={onFinish} autoComplete="off">
-        <Form.Item<FieldType>
-          label={<span className="font-light text-gray-500 dark:text-gray-400">Full Name</span>}
-          name="full_name"
-          rules={[{ required: true, message: "Required" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label={<span className="font-light text-gray-500 dark:text-gray-400">Email</span>}
-          name="email"
-          rules={[
-            { required: true, message: "Email Required" },
-            { type: "email", message: "Invalid email" },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+    <div className="w-2/5">
+      <h1 className="text-2xl justify-center grid mb-4">Registeration</h1>
+      <div className="bg-slate-600 rounded-lg  border p-4">
+        {contextHolder}
+        <Form className="space-y-4 md:space-y-6" layout="vertical" name="registrationForm" onFinish={onFinish} autoComplete="off">
+          <Form.Item<FieldType>
+            label={<span className="font-light text-gray-500 dark:text-gray-400">Full Name</span>}
+            name="full_name"
+            rules={[{ required: true, message: "Required" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label={<span className="font-light text-gray-500 dark:text-gray-400">Email</span>}
+            name="email"
+            rules={[
+              { required: true, message: "Email Required" },
+              { type: "email", message: "Invalid email" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item<FieldType>
-          label={<span className="font-light text-gray-500 dark:text-gray-400">Password</span>}
-          name="password"
-          rules={[{ required: true, message: "Password Required" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item<FieldType>
+            label={<span className="font-light text-gray-500 dark:text-gray-400">Password</span>}
+            name="password"
+            rules={[{ required: true, message: "Password Required" }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" danger block>
-            Register
-          </Button>
-        </Form.Item>
-        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-          <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
-            Login
-          </Link>
-        </p>
-      </Form>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" danger block>
+              Register
+            </Button>
+          </Form.Item>
+          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+            <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+              Login
+            </Link>
+          </p>
+        </Form>
+      </div>
     </div>
   );
 });
